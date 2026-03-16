41868c4d72f34bc84238a31d34cca908119db590
ChavezXXL <CHAVEZ.A21@ICLOUD.COM>
2026-03-16 08:51:36 -0700

    Add optional specialInstructions field to types

--- types.ts
+++ types.ts
@@ -24,6 +24,7 @@ export interface Job {
   dateReceived: string;
   dueDate: string;
   info: string;
+  specialInstructions?: string;
   status: JobStatus;
   createdAt: number;
   completedAt?: number;
